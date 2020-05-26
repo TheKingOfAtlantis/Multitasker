f10902537f3810c45a987c3e9f4d2866a56c8fbd
Sulaiman Sulaiman <SKSulai98@gmail.com>
2020-05-26 18:42:06 +0100

    UserModel properties updated
    Many have now been made optional to provide additional flexibility in how much
    information we have from firebase
    
    Epic: #5, #6
    Issue: #39
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/model/UserModel.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/model/UserModel.kt
@@ -8,12 +8,12 @@ import java.time.LocalDate
 @Entity data class UserModel(
     @PrimaryKey val ID: String, // ID of the user
     val FirebaseID: String?,    // Firebase uid for the User
-    val DisplayName: String,    // User display name
-    val Email: String,          // Email associated with the account
+    val DisplayName: String?,   // User display name
+    val Email: String?,         // Email associated with the account
 
     // Additional user info
     val Avatar: Uri?,           // Avatar icon
-    val ActualName: String,     // User's IRL name
+    val ActualName: String?,    // User's IRL name
     val Home: String?,          // Address of the user's home
-    val DOB: LocalDate?     // Date of Birth
+    val DOB: LocalDate?         // Date of Birth
 )
