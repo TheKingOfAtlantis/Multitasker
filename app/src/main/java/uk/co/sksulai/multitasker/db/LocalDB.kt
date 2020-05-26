17189694f57aed4676c0b9e41a7635c30b6e1071
Sulaiman Sulaiman <SKSulai98@gmail.com>
2020-05-26 14:04:29 +0100

    Database: User Model
    Added:
     - UserModel w/ following properties:
         - ID
         - Firebase UID
         - Display Name,
         - Email
         - Avatar
         - Actual Name
         - Home
         - Date of Birth
     - UserModel in Database entities list
    
    Epic: #5, #6
    Issue: #39
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/LocalDB.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/LocalDB.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.*
 
 import uk.co.sksulai.multitasker.db.converter.*
+import uk.co.sksulai.multitasker.db.model.*
 
 inline fun <reified DB : RoomDatabase> databaseBuilder(context: Context, name: String)
         = Room.databaseBuilder(context, DB::class.java, name)
@@ -16,6 +17,7 @@ fun LocalDB.Companion.createDatabase(context: Context)
     DateTimeConverter::class,
     UriConverter::class
 ) @Database(version = 1, entities = [
+    UserModel::class
 ]) abstract class LocalDB : RoomDatabase() {
     companion object { const val DBName = "Multitasker.db" }
 }
---
+++ app/src/main/java/uk/co/sksulai/multitasker/db/model/UserModel.kt
@@ -0,0 +1,19 @@
+package uk.co.sksulai.multitasker.db.model
+
+import android.net.Uri
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity data class UserModel(
+    @PrimaryKey val ID: String, // ID of the user
+    val FirebaseID: String?,    // Firebase uid for the User
+    val DisplayName: String,    // User display name
+    val Email: String,          // Email associated with the account
+
+    // Additional user info
+    val Avatar: Uri?,           // Avatar icon
+    val ActualName: String,     // User's IRL name
+    val Home: String?,          // Address of the user's home
+    val DOB: LocalDate?     // Date of Birth
+)
