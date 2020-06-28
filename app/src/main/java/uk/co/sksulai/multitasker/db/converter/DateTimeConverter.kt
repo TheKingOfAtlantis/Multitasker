bfc97ab087f877b15b6320052426713fa9abbbf1
Sulaiman Sulaiman <SKSulai98@gmail.com>
2020-06-28 13:47:15 +0100

    Misc Time Related Converters
    Added:
     - Converter for TimeZones
     - Converter for Duration
    
    Epic: #38
    Issue: #37
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/converter/DateTimeConverter.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/converter/DateTimeConverter.kt
@@ -2,6 +2,7 @@ package uk.co.sksulai.multitasker.db.converter
 
 import androidx.room.TypeConverter
 
+import java.util.*
 import java.time.*
 import java.time.format.DateTimeFormatter
 
@@ -16,8 +17,18 @@ class DateConverter : IConverter<LocalDate?, String?> {
     @TypeConverter override fun from(value: LocalDate?): String? = value?.let { formatter.format(it) }
     @TypeConverter override fun to(value: String?): LocalDate? = value?.let { formatter.parse(it, LocalDate::from) }
 }
+
+// Misc converter
+
+class TimeZoneConverter: IConverter<TimeZone?, String?> {
+    @TypeConverter override fun from(value: TimeZone?): String? = value?.let { value.id }
+    @TypeConverter override fun to(value: String?): TimeZone? = value?.let { TimeZone.getTimeZone(it) }
 }
 class LocalTimeConverter: IConverter<LocalTime?, String?> {
     @TypeConverter override fun from(value: LocalTime?): String? = if(value == null) null else value.toString()
     @TypeConverter override fun to(value: String?): LocalTime? = value?.let { LocalTime.parse(it) }
+}
+class DurationConverter: IConverter<Duration?, String?> {
+    @TypeConverter override fun from(value: Duration?): String? = value?.toString()
+    @TypeConverter override fun to(value: String?): Duration? = Duration.parse(value)
 }
