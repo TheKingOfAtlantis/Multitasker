823ea5a28d0307c5cd3857dbd126ea73bcddab65
Sulaiman Sulaiman <SKSulai98@gmail.com>
2020-06-28 13:44:04 +0100

    Date Converter uses ISO_LOCAL_DATE formatter
    Epic: #38
    Issue: #37
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/converter/DateTimeConverter.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/converter/DateTimeConverter.kt
@@ -1,15 +1,21 @@
 package uk.co.sksulai.multitasker.db.converter
 
 import androidx.room.TypeConverter
+
 import java.time.*
+import java.time.format.DateTimeFormatter
 
 class DateTimeConverter : IConverter<LocalDateTime?, String?> {
     @TypeConverter override fun from(value: LocalDateTime?): String? = if(value == null) null else value.toString()
     @TypeConverter override fun to(value: String?): LocalDateTime? = value?.let { LocalDateTime.parse(it) }
 }
+// Date converter
+
 class DateConverter : IConverter<LocalDate?, String?> {
-    @TypeConverter override fun from(value: LocalDate?): String? = if(value == null) null else value.toString()
-    @TypeConverter override fun to(value: String?): LocalDate? = value?.let { LocalDate.parse(it) }
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
+    @TypeConverter override fun from(value: LocalDate?): String? = value?.let { formatter.format(it) }
+    @TypeConverter override fun to(value: String?): LocalDate? = value?.let { formatter.parse(it, LocalDate::from) }
+}
 }
 class LocalTimeConverter: IConverter<LocalTime?, String?> {
     @TypeConverter override fun from(value: LocalTime?): String? = if(value == null) null else value.toString()
