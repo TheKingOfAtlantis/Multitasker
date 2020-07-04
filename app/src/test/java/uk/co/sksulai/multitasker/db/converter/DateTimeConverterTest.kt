8acf8f3146ce36abdc8210a29bfad10043e44a0d
Sulaiman Sulaiman <SKSulai98@gmail.com>
2020-07-04 14:05:21 +0100

    OffsetDateTime Converter
    Epic: #38
    Issue: #37
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/test/java/uk/co/sksulai/multitasker/db/converter/DateTimeConverterTest.kt
+++ app/src/test/java/uk/co/sksulai/multitasker/db/converter/DateTimeConverterTest.kt
@@ -5,6 +5,24 @@ import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 import java.time.*
+
+class OffsetDateTimeConverterTest : ConverterTest<OffsetDateTime, String>(
+    OffsetDateTimeConverter(),
+    listOf(
+        OffsetDateTime.of(
+            LocalDateTime.of(
+                LocalDate.of(2020, 6, 29),
+                LocalTime.of(10, 10, 10)
+            ),
+            ZoneOffset.ofHours(1)
+        ) to "2020-06-29T10:10:10+01:00"
+    )
+){
+    @Test override fun withNull() = super.withNull()
+    @Test override fun validateConversion() = super.validateConversion()
+    @Test override fun inverse() = super.inverse()
+}
+
 class LocalDateTimeConverterTest : ConverterTest<LocalDateTime, String>(
     LocalDateTimeConverter(),
     listOf(
