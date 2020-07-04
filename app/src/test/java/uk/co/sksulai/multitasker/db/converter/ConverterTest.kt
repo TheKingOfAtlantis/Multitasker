585725d882b1b2cc400eb6d6a6fc13f95ca992ea
Sulaiman Sulaiman <SKSulai98@gmail.com>
2020-07-04 13:47:36 +0100

    Matcher Parameters Fixed
    Previously the actual and expected parameters did not match with what was being
    supplied to them, rearranged code so that when a test fails and it prints out
    the actual/expected values that they actually are what was the actual/expected
    values rather than being the wrong way round
    
    Epic: #38
    Issue: #37
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/test/java/uk/co/sksulai/multitasker/db/converter/ConverterTest.kt
+++ app/src/test/java/uk/co/sksulai/multitasker/db/converter/ConverterTest.kt
@@ -2,7 +2,6 @@ package uk.co.sksulai.multitasker.db.converter
 
 import org.hamcrest.Matchers
 import org.junit.Assert
-import org.junit.Test
 
 abstract class ConverterTest<T, U>(
     protected val converter : IConverter<T?, U?>,
@@ -10,22 +9,24 @@ abstract class ConverterTest<T, U>(
 ) {
     open fun withNull() {
         Assert.assertThat(
-            null,
-            Matchers.equalTo(converter.from(null))
+            converter.from(null),
+            Matchers.equalTo<U?>(null)
         )
         Assert.assertThat(
-            null,
-            Matchers.equalTo(converter.to(null))
+            converter.to(null),
+            Matchers.equalTo<T?>(null)
         )
     }
     open fun validateConversion() {
         testData.forEach { (lhs, rhs) ->
-            Assert.assertThat(rhs, Matchers.equalTo(
-                converter.from(lhs)
-            ))
-            Assert.assertThat(lhs, Matchers.equalTo(
-                converter.to(rhs)
-            ))
+            Assert.assertThat(
+                converter.from(lhs),
+                Matchers.equalTo(rhs)
+            )
+            Assert.assertThat(
+                converter.to(rhs),
+                Matchers.equalTo(lhs)
+            )
         }
     }
     open fun inverse() {
@@ -33,15 +34,17 @@ abstract class ConverterTest<T, U>(
         //     from(to(x)) == x
         //     to(from(y)) == y
         testData.forEach { (lhs, rhs) ->
-            Assert.assertThat(lhs?.toString(), Matchers.equalTo(
-                converter.from(converter.to(rhs))?.toString()
-            ))
+            Assert.assertThat(
+                converter.from(converter.to(rhs))?.toString(),
+                Matchers.equalTo(lhs?.toString())
+            )
             // Fixme: Find work around ?.toString()
             //        Without it values don't match even tho the values are equivalent
             //        but one is surrounded by <...> and the other "..."
-            Assert.assertThat(rhs?.toString(), Matchers.equalTo(
-                converter.to(converter.from(lhs))?.toString()
-            ))
+            Assert.assertThat(
+                converter.to(converter.from(lhs))?.toString(),
+                Matchers.equalTo(rhs?.toString())
+            )
         }
     }
 }
