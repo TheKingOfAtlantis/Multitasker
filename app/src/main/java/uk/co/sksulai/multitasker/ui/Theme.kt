62e1a6c61c27508f1b475ffc5cf2b080e4ffe67f
Sulaiman Sulaiman <SKSulai98@gmail.com>
2020-07-05 18:55:04 +0100

    Multitasker Colour Palettes
    Added:
     - Light theme palette
     - Dark theme palette
    
    Issue: #43
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

---
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/Theme.kt
@@ -0,0 +1,32 @@
+package uk.co.sksulai.multitasker.ui
+
+import androidx.ui.graphics.Color
+import androidx.ui.material.*
+
+object MultitaskerColour {
+    object Palette {
+        val Primary     = Color(0xff8636b2)
+        val LightAccent = Color(0xffb784a9)
+        val DarkAccent  = Color(0xff647098)
+        val DarkShades  = Color(0xff1e1e34)
+
+        val Info     = Color(0xff20213a)
+        val Success  = Color(0x5d8b6d)
+        val Warning  = Color(0xdb7b35)
+        val Danger   = Color(0xf44336)
+    }
+}
+
+object MultitaskerTheme {
+    private val darkTheme = darkColorPalette(
+        primary        = MultitaskerColour.Palette.Primary,
+        primaryVariant = MultitaskerColour.Palette.DarkAccent,
+        secondary      = MultitaskerColour.Palette.LightAccent,
+        error          = MultitaskerColour.Palette.Danger
+    )
+    private val lightTheme = lightColorPalette(
+        primary        = MultitaskerColour.Palette.Primary,
+        primaryVariant = MultitaskerColour.Palette.DarkAccent,
+        error          = MultitaskerColour.Palette.Danger
+    )
+}
