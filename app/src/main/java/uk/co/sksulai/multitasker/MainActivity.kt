81e392e2c4bd54b943c77c5bd3e3aa48a0a66513
Sulaiman Sulaiman <SKSulai98@gmail.com>
2020-07-06 15:30:51 +0100

    Apply the Multitasker Theme
    Issue: #43
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/MainActivity.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/MainActivity.kt
@@ -2,14 +2,19 @@ package uk.co.sksulai.multitasker
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.ui.core.setContent
 
+import androidx.ui.core.setContent
+import androidx.ui.material.MaterialTheme
+
+import uk.co.sksulai.multitasker.ui.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            MaterialTheme(MultitaskerTheme.currentTheme) {
 
+            }
         }
     }
 }
