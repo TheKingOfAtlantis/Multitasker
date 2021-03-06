5e4794225153c6f0e385c761f08dc92a0ac81196
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-03-06 19:18:44 +0000

    Sign-in Basic Implementation
    Added:
     - SignIn EntryPoint: Optionally takes in email & password, these can be used to
       allow us to automatically fill in email and password fields
     - User ViewModel: Performs logic to authenticate the user and handle exceptions
       in an approprate manner
    
    Epic: #50
    Issue: #45
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/viewmodel/UserViewModel.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/viewmodel/UserViewModel.kt
@@ -2,10 +2,55 @@ package uk.co.sksulai.multitasker.db.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.google.firebase.auth.FirebaseAuthException
 import uk.co.sksulai.multitasker.db.repo.UserRepository
+
+typealias emailError = suspend (emailError: String, passwordError: String, authError: String) -> Unit
 
 class UserViewModel(app: Application) : AndroidViewModel(app) {
     private val userRepo by lazy { UserRepository(app) }
 
     val currentUser = userRepo.currentUser
+
+    private suspend fun handleAuthError(
+        err: FirebaseAuthException,
+        onError: emailError
+    ) {
+        // Copied from StackOverflow: https://stackoverflow.com/a/48503254/3856359
+        val authError = when (err.errorCode) {
+            "ERROR_INVALID_CUSTOM_TOKEN"                     -> "The custom token format is incorrect. Please check the documentation."
+            "ERROR_CUSTOM_TOKEN_MISMATCH"                    -> "The custom token corresponds to a different audience."
+            "ERROR_INVALID_CREDENTIAL"                       -> "The supplied auth credential is malformed or has expired."
+            "ERROR_INVALID_EMAIL"                            -> "The email address is badly formatted."
+            "ERROR_WRONG_PASSWORD"                           -> "The password is invalid or the user does not have a password."
+            "ERROR_USER_MISMATCH"                            -> "The supplied credentials do not correspond to the previously signed in user."
+            "ERROR_REQUIRES_RECENT_LOGIN"                    -> "This operation is sensitive and requires recent authentication. Log in again before retrying this request."
+            "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" -> "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address."
+            "ERROR_EMAIL_ALREADY_IN_USE"                     -> "The email address is already in use by another account."
+            "ERROR_CREDENTIAL_ALREADY_IN_USE"                -> "This credential is already associated with a different user account."
+            "ERROR_USER_DISABLED"                            -> "The user account has been disabled by an administrator."
+            "ERROR_USER_TOKEN_EXPIRED"                       -> "The user\'s credential is no longer valid. The user must sign in again."
+            "ERROR_USER_NOT_FOUND"                           -> "There is no user record corresponding to this identifier. The user may have been deleted."
+            "ERROR_INVALID_USER_TOKEN"                       -> "The user\'s credential is no longer valid. The user must sign in again."
+            "ERROR_OPERATION_NOT_ALLOWED"                    -> "This operation is not allowed. You must enable this service in the console."
+            "ERROR_WEAK_PASSWORD"                            -> "The password is invalid it must 6 characters at least"
+            else -> throw err // We couldn't provide user friendly explanation for exception (Must be something fatal or something new)
+        }
+        // Some error codes should be given as email or password errors instead
+        when(err.errorCode) {
+            "ERROR_INVALID_EMAIL",
+            "ERROR_EMAIL_ALREADY_IN_USE" -> onError(authError, "", "")
+            "ERROR_WRONG_PASSWORD",
+            "ERROR_WEAK_PASSWORD" -> onError("", authError, "")
+            else -> onError("", "", authError)
+        }
+    }
+
+    suspend fun authenticate(email: String, password: String, onError: suspend (emailError: String, passwordError: String, authError: String) -> Unit) = when {
+        email.isEmpty()    -> { onError("No email provided", "", "") }
+        password.isEmpty() -> { onError("", "No password provided", "") }
+        else ->
+        try { userRepo.authenticate(email, password) }
+        catch (e: FirebaseAuthException) { handleAuthError(e, onError) }
+    }
 }
--- app/src/main/java/uk/co/sksulai/multitasker/ui/userFlow/SignInScreen.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/userFlow/SignInScreen.kt
@@ -3,10 +3,77 @@ package uk.co.sksulai.multitasker.ui.userFlow
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.*
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+import androidx.navigation.compose.navigate
+import kotlinx.coroutines.launch
+import uk.co.sksulai.multitasker.db.viewmodel.UserViewModel
 import uk.co.sksulai.multitasker.util.LocalNavController
 
-@Composable fun SignInScreen() {
+@Composable fun SignInScreen(emailIn: String = "", pwdIn: String = "") {
     val navController = LocalNavController.current
+
+    val userViewModel = viewModel<UserViewModel>()
+    val scaffoldState = rememberScaffoldState()
+    val scope         = rememberCoroutineScope()
+
+    Scaffold(
+        Modifier.fillMaxSize(),
+        scaffoldState
+    ) { Column(
+        Modifier.fillMaxSize(),
+        Arrangement.Center,
+        Alignment.CenterHorizontally
+    ) {
+        var email    by rememberSaveable { mutableStateOf(emailIn) }
+        var password by rememberSaveable { mutableStateOf(pwdIn) }
+
+        var emailError    by rememberSaveable { mutableStateOf("") }
+        var passwordError by rememberSaveable { mutableStateOf("") }
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label   = { Text("Email") },
+            isError = emailError.isNotEmpty()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = { TransformedText(
+                text = AnnotatedString("*".repeat(it.text.length)),
+                offsetMapping = OffsetMapping.Identity
+            ) },
+            isError = passwordError.isNotEmpty()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Button(onClick = { scope.launch {
+                userViewModel.authenticate(email, password) { emailErr, passwordErr, authErr ->
+                    emailError    = emailErr
+                    passwordError = passwordErr
+
+                    if(authErr.isNotEmpty())
+                        scaffoldState.snackbarHostState.showSnackbar(authErr)
+                }
+            }
+            }) { Text("Sign In") }
+            Spacer(modifier = Modifier.width(8.dp))
+            Button(onClick = {
+                navController.navigate("SignUp" + when {
+                    email.isNotEmpty()    -> "?email=${email}"
+                    password.isNotEmpty() -> "${if(email.isEmpty()) "?" else "&"}password=${password}"
+                    else -> ""
+                })
+            }) { Text("Sign Up") }
+        }
+    } }
 }
