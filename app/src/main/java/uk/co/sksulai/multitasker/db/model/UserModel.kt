bfad1a28246041f9bc84815588b3f3dbeee4fe69
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-03-24 14:56:33 +0000

    Preferred Home Screen
    The user can select which screen they would prefer to be their default home, may
    in the future change to a map to allow for different preferences depending on the
    platform (i.e. web or mobile)
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/model/UserModel.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/model/UserModel.kt
@@ -1,13 +1,12 @@
 package uk.co.sksulai.multitasker.db.model
 
 import android.net.Uri
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import java.time.Instant
-import java.time.LocalDate
+import androidx.room.*
+import java.time.*
 
 @Entity data class UserModel(
     @PrimaryKey val ID: String, // ID of the user - Same as Firebase uid
+
     // Metadata
     val Creation: Instant,      // User creation timestamp
     val LastModified: Instant,  // When user last modified
@@ -16,6 +15,7 @@ import java.time.LocalDate
     val Email: String?,         // Email associated with the account
 
     // Additional user info
+    val PreferredHome: String,  // User's preferred "home" screen
     val Avatar: Uri?,           // Avatar icon
     val ActualName: String?,    // User's IRL name
     val Home: String?,          // Address of the user's home
