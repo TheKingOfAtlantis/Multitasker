c583d7c84d9b148f5c502ab921268861143ba043
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-03-24 14:24:10 +0000

    Datastore Security Rule Fix
    Fixed:
     -  Issue due to the security rules blocking all read/write requests, wasn't
        adjusted to allow user to access their document
     - UserRepo was accessing the current user on the Main Thread
    Updated:
     - ID uses firebase UID
     - Collection for users renamed: user => users
     - Database version incremented
     - Database insertion of a user will replace value with incoming value
    Removed:
     - ID from the Firestore document (its also in the document name)
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/LocalDB.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/LocalDB.kt
@@ -10,7 +10,9 @@ import uk.co.sksulai.multitasker.db.dao.*
 inline fun <reified DB : RoomDatabase> databaseBuilder(context: Context, name: String)
         = Room.databaseBuilder(context, DB::class.java, name)
 fun LocalDB.Companion.createDatabase(context: Context)
-        = databaseBuilder<LocalDB>(context, DBName).build()
+        = databaseBuilder<LocalDB>(context, DBName)
+            .fallbackToDestructiveMigration()
+            .build()
 
 @TypeConverters(
     ZonedDateTimeConverter::class,
@@ -22,7 +24,7 @@ fun LocalDB.Companion.createDatabase(context: Context)
     TimeZoneConverter::class,
     DurationConverter::class,
     UriConverter::class
-) @Database(version = 1, entities = [
+) @Database(version = 2, entities = [
     UserModel::class
 ]) abstract class LocalDB : RoomDatabase() {
     companion object { const val DBName = "Multitasker.db" }
