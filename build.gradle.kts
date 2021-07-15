3d1d09825980551c8bd704e808113b00b4ad4742
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-07-15 13:34:43 +0100

    Updated Dependencies
    Added:
     - Configuration Caching
     - Incremental Kapt compilation
     - Parallel GC
     - Filesystem Watching
     - Dependencies:
         - Firebase:
             - Crashlytics
             - Dynamic Links
         - Compose Accompanist:
             - Pager
             - Placeholder
             - Permissions
             - System UI Controller
    Updated:
     - Syntax for defining android build versions have changed
     - Dependencies:
         - Gradle Wrapper: 6.8.2 => 7.0.2
         - Gradle Plugins:
             - Android Gradle Build Plugin: 7.0.0-alpha08 => 7.0.0-beta05
             - Kotlin Gradle Plugin: 1.4.30 => 1.5.20 (Now we can use any version with compose)
         - Android Versions: Build Tools - 30.0.2 => 31.0.0-rc2
         - Compose accompanist now maintained by google
         - See commit for rest
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha08")
-        classpath(kotlin("gradle-plugin", version = "1.4.30"))
-        classpath("com.google.gms:google-services:4.3.5")
+        classpath("com.android.tools.build:gradle:7.0.0-beta05")
+        classpath(kotlin("gradle-plugin", version = "1.5.20"))
+        classpath("com.google.gms:google-services:4.3.8")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -23,6 +24,6 @@ allprojects {
     }
 }
 
-tasks.register("clean", Delete::class){
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
