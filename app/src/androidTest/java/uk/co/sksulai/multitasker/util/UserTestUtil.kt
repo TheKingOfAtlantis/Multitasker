56961429a30602d1578c12ab16b6c53b0ef74c64
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-12-20 13:34:36 +0000

    Testing Random Utility: Strings & Emails
    Created a random string and email utility to be used for creating random
    authentication details for testing the user repository
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/androidTest/java/uk/co/sksulai/multitasker/util/UserTestUtil.kt
+++ app/src/androidTest/java/uk/co/sksulai/multitasker/util/UserTestUtil.kt
@@ -1,8 +1,23 @@
 package uk.co.sksulai.multitasker.util
 
-import uk.co.sksulai.multitasker.db.model.UserModel
-import java.time.Instant
 import java.util.*
+import java.time.Instant
+
+import kotlin.random.Random
+
+import uk.co.sksulai.multitasker.db.model.UserModel
+
+object RandomUtil {
+    private val DefaultCharSet = (('a'..'z') + ('A'..'Z') + ('0'..'9'))
+
+    fun nextString(min: Int, max: Int, allowed: List<Char> = DefaultCharSet) = nextString(Random.nextInt(min, max), allowed)
+    fun nextString(length: Int, allowed: List<Char> = DefaultCharSet): String =
+        IntRange(0, length)
+            .map { Random.nextInt(until = DefaultCharSet.size) }
+            .map(DefaultCharSet::get)
+            .joinToString("")
+    fun nextEmail(domain: String = "domain.com") = "${nextString(4, 12)}@$domain"
+}
 
 object UserTestUtil {
     fun createSingle() = UserModel(
