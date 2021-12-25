4ea1a41a066c0b2d96c00fdb403e3c08e0d84376
Sulaiman Sulaiman <SKSulai98@gmail.com>
2021-12-25 21:38:51 +0000

    Added missing Retrofit Dependency
    Retrofit is needed for FirebaseEmulatorUtil but forgot to commit the change
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,10 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.38.1")
     kapt("com.google.dagger:hilt-android-compiler:2.38.1")
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     // Glide
     implementation("com.github.bumptech.glide:glide:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
