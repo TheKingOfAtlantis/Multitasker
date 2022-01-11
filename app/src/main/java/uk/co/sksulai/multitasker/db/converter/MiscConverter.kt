aceab314a0bf44f501e98c4c8c485dfe69ececd0
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-11 23:15:55 +0000

    TypeConverters: Replaced Lambdas w/ Reference
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/converter/MiscConverter.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/converter/MiscConverter.kt
@@ -5,5 +5,5 @@ import androidx.room.TypeConverter
 
 object UriConverter : IConverter<Uri?, String?> {
     @TypeConverter override fun from(value: Uri?): String? = value?.toString()
-    @TypeConverter override fun to(value: String?): Uri?   = value?.let { Uri.parse(it) }
+    @TypeConverter override fun to(value: String?): Uri?   = value?.let(Uri::parse)
 }
