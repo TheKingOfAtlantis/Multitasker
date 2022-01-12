27e1e936a17fe3cf3232d452968f1cf593135ef3
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-12 02:05:47 +0000

    Navigation Destination Base Classes
    Added:
     - Core Destination class: Provide a strongly typed representation of a route
     - Extended Destination class: Destination with a name/title and icon
     - Destinations object: Contains all our destinations
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

---
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/Destinations.kt
@@ -0,0 +1,45 @@
+package uk.co.sksulai.multitasker.ui
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.NavOptionsBuilder
+
+
+/**
+ * Provides a strongly typed representation of a navigation route
+ * @param route Route to the destination
+ */
+sealed class Destination(val route: String) {
+    /**
+     * Navigates to this destination using a given [NavController]
+     * @param navController The controller to use to get to this destination
+     * @param builder Optional DSL to specify navigation options
+     */
+    fun navigate(
+        navController: NavController,
+        builder: (NavOptionsBuilder.() -> Unit)? = null
+    ) = builder?.let { navController.navigate(route, it) } ?: navController.navigate(route)
+}
+/**
+ * Provides a strongly typed representation of a navigation route with additional
+ * info including a title and icon
+ * @param route Route to the destination
+ * @param title String resource containing the name/title for this destination
+ * @param icon  Icon to be used to represent this destination
+ */
+sealed class ExtendedDestination(
+    route: String,
+    @StringRes val title: Int,
+    val icon: ImageVector
+) : Destination(route) {
+    /**
+     * Retrieves the value of the title from the string resource
+     */
+    @Composable fun getTitleValue() = stringResource(title)
+}
+
+object Destinations {
+}
