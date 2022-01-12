6c4dc89aedbb87cc27eaa332b342f0ebd58f662e
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-12 01:06:37 +0000

    Refactor MultitaskerColour
    Removed the unnecessarily nested objects: MultitaskerColour.Palette => MultitaskerPalette
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/ui/Theme.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.*
 import androidx.compose.foundation.*
 import androidx.compose.ui.graphics.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
 
 import uk.co.sksulai.multitasker.db.AppSettings
 
@@ -14,43 +15,42 @@ enum class ThemeState {
     Dark
 }
 
-object MultitaskerColour {
-    object Palette {
-        val Primary     = Color(0xff8636b2)
-        val LightAccent = Color(0xffb784a9)
-        val DarkAccent  = Color(0xff647098)
-        val DarkShades  = Color(0xff1e1e34)
+object MultitaskerPalette {
+    val Primary     = Color(0xff8636b2)
+    val LightAccent = Color(0xffb784a9)
+    val DarkAccent  = Color(0xff647098)
+    val DarkShades  = Color(0xff1e1e34)
 
-        val Info        = Color(0xff20213a)
-        val Success     = Color(0xff5d8b6d)
-        val Warning     = Color(0xffdb7b35)
-        val Danger      = Color(0xfff44336)
-    }
+    val Info        = Color(0xff20213a)
+    val Success     = Color(0xff5d8b6d)
+    val Warning     = Color(0xffdb7b35)
+    val Danger      = Color(0xfff44336)
 }
 
-val Colors.Info: Color    get() = MultitaskerColour.Palette.Info
-val Colors.Success: Color get() = MultitaskerColour.Palette.Success
-val Colors.Warning: Color get() = MultitaskerColour.Palette.Warning
+val Colors.Info: Color    get() = MultitaskerPalette.Info
+val Colors.Success: Color get() = MultitaskerPalette.Success
+val Colors.Warning: Color get() = MultitaskerPalette.Warning
 
 object MultitaskerTheme {
     val darkTheme = darkColors(
-        primary        = MultitaskerColour.Palette.Primary,
-        primaryVariant = MultitaskerColour.Palette.DarkAccent,
-        secondary      = MultitaskerColour.Palette.LightAccent,
-        error          = MultitaskerColour.Palette.Danger
+        primary        = MultitaskerPalette.Primary,
+        primaryVariant = MultitaskerPalette.DarkAccent,
+        secondary      = MultitaskerPalette.LightAccent,
+        error          = MultitaskerPalette.Danger
     )
     val lightTheme = lightColors(
-        primary        = MultitaskerColour.Palette.Primary,
-        primaryVariant = MultitaskerColour.Palette.DarkAccent,
-        error          = MultitaskerColour.Palette.Danger
+        primary        = MultitaskerPalette.Primary,
+        primaryVariant = MultitaskerPalette.DarkAccent,
+        secondary      = MultitaskerPalette.LightAccent,
+        error          = MultitaskerPalette.Danger
     )
     val shapes = Shapes(
-        small = RoundedCornerShape(50)
+        small  = RoundedCornerShape(50),
+        medium = RoundedCornerShape(16.dp)
     )
 }
 
 @Composable fun MultitaskerTheme(content: @Composable () -> Unit) {
-
     val themePref = AppSettings.General.theme.let { pref -> // Get stored preference
         if(pref == ThemeState.System) // If to just follow the system then we need to detect what it currently is
             if(isSystemInDarkTheme())
@@ -65,8 +65,8 @@ object MultitaskerTheme {
     }
 
     MaterialTheme(
-        colors = theme,
-        shapes = MultitaskerTheme.shapes,
+        colors  = theme,
+        shapes  = MultitaskerTheme.shapes,
         content = content
     )
 }
