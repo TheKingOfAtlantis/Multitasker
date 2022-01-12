0d93e06a9f0cbe495e7c24e34be6dffc0f0fabcf
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-12 00:14:25 +0000

    Major changes to dependencies
    Added:
     - Test coverage for debug build
     - Dependency version variables
     - New dependencies:
         - kotlin-parcelize: Just makes adding parcelizable easier
         - Coil: for image loading from url
         - androidx.window: So we can adjust size
         - Most accompanist libraries
    Updated:
     - Dependency versions to latest
     - Increased jvm memory: 2048 => 4096
    Fixed:
     - Disabled kapt worker: Causing issues with Hilt
    Removed:
     - kotlinOptions.useIR has been deprecated
     - Glide replaced w/ coil as it has native compose functions
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,26 @@
+
+object Version {
+    const val hilt        = "2.40.5"
+    const val lifecycle   = "2.4.0"
+    const val workmanager = "2.7.1"
+    const val datastore   = "1.0.0"
+    const val room        = "2.4.0"
+
+    const val compose     = "1.1.0-rc01"
+    const val accompanist = "0.22.0-rc"
+    const val nav         = "2.4.0-rc01"
+
+    const val testing     = "1.4.1-alpha03"
+    const val espresso    = "3.5.0-alpha03"
+
+    const val retrofit    = "2.9.0"
+}
+
 plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("kapt")
+    id("kotlin-parcelize")
     id("dagger.hilt.android.plugin")
     id("com.google.gms.google-services")
     id("com.google.firebase.crashlytics")
@@ -27,6 +46,7 @@ android {
     buildTypes {
         getByName("debug") {
             isMinifyEnabled = false
+            isTestCoverageEnabled = true
         }
         getByName("release") {
             isMinifyEnabled = true
@@ -39,7 +59,6 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
-        useIR     = true
         freeCompilerArgs += listOf(
             "-Xskip-prerelease-check",
             "-XXLanguage:+InlineClasses",
@@ -67,6 +86,10 @@ android {
         density  { enableSplit = true }
         abi      { enableSplit = true }
     }
+
+    useLibrary("android.test.runner")
+    useLibrary("android.test.base")
+    useLibrary("android.test.mock")
 }
 
 kapt {
@@ -87,48 +110,50 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.5.2-native-mt")
 
     // Hilt
-    implementation("com.google.dagger:hilt-android:2.38.1")
-    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
+    implementation("com.google.dagger:hilt-android:${Version.hilt}")
+    kapt("com.google.dagger:hilt-android-compiler:${Version.hilt}")
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:${Version.retrofit}")
+    implementation("com.squareup.retrofit2:converter-gson:${Version.retrofit}")
 
-    // Glide
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    kapt("com.github.bumptech.glide:compiler:4.12.0")
+    // Coil
+    implementation("io.coil-kt:coil-compose:1.4.0")
 
     // Androidx
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.0-rc01")
+    implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.window:window:1.0.0-rc01")
 
     // Jetpack Compose
-    implementation("androidx.compose.ui:ui:1.1.0-beta02")
-    implementation("androidx.compose.ui:ui-tooling:1.1.0-beta02")
-    implementation("androidx.compose.foundation:foundation:1.1.0-beta02")
-    implementation("androidx.compose.material:material:1.1.0-beta02")
-    implementation("androidx.compose.material:material-icons-core:1.1.0-beta02")
-    implementation("androidx.compose.material:material-icons-extended:1.1.0-beta02")
-    implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
-    implementation("androidx.compose.runtime:runtime-livedata:1.1.0-beta02")
+    implementation("androidx.compose.ui:ui:${Version.compose}")
+    implementation("androidx.compose.ui:ui-test:${Version.compose}")
+    implementation("androidx.compose.ui:ui-tooling:${Version.compose}")
+    implementation("androidx.compose.foundation:foundation:${Version.compose}")
+    implementation("androidx.compose.material:material:${Version.compose}")
+    implementation("androidx.compose.material:material-icons-core:${Version.compose}")
+    implementation("androidx.compose.material:material-icons-extended:${Version.compose}")
 
-    implementation("androidx.compose.foundation:foundation-layout:1.1.0-beta02")
-    implementation("androidx.compose.animation:animation:1.1.0-beta02")
+    implementation("androidx.compose.foundation:foundation-layout:1.0.5")
+    implementation("androidx.compose.animation:animation:1.1.0-rc01")
 
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-rc01")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${Version.lifecycle}")
 
-    implementation("com.google.accompanist:accompanist-glide:0.15.0")
-    implementation("com.google.accompanist:accompanist-insets:0.13.0")
-
-    implementation("androidx.compose.ui:ui-tooling:1.1.0-beta02")
-    implementation("androidx.compose.ui:ui-test:1.1.0-beta02")
+    implementation("com.google.accompanist:accompanist-insets:${Version.accompanist}")
+    implementation("com.google.accompanist:accompanist-insets-ui:${Version.accompanist}")
+    implementation("com.google.accompanist:accompanist-pager:${Version.accompanist}")
+    implementation("com.google.accompanist:accompanist-pager-indicators:${Version.accompanist}")
+    implementation("com.google.accompanist:accompanist-placeholder-material:${Version.accompanist}")
+    implementation("com.google.accompanist:accompanist-permissions:${Version.accompanist}")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:${Version.accompanist}")
+    implementation("com.google.accompanist:accompanist-drawablepainter:${Version.accompanist}")
+    implementation("com.google.accompanist:accompanist-navigation-material:0.22.1-SNAPSHOT")
 
     // Firebase & Google
-    implementation("com.google.android.gms:play-services-auth:19.2.0")
+    implementation("com.google.android.gms:play-services-auth:20.0.1")
 
-    implementation(platform("com.google.firebase:firebase-bom:29.0.0"))
+    implementation(platform("com.google.firebase:firebase-bom:29.0.3"))
     implementation("com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava")
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
@@ -139,42 +164,45 @@ dependencies {
 
 
     // Arch: Lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-service:2.4.0")
-    implementation("androidx.lifecycle:lifecycle-process:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${Version.lifecycle}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.lifecycle}")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${Version.lifecycle}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:${Version.lifecycle}")
+    implementation("androidx.lifecycle:lifecycle-common-java8:${Version.lifecycle}")
+    implementation("androidx.lifecycle:lifecycle-service:${Version.lifecycle}")
+    implementation("androidx.lifecycle:lifecycle-process:${Version.lifecycle}")
 
     // Arch: DataStore
-    implementation("androidx.datastore:datastore:1.0.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore:${Version.datastore}")
+    implementation("androidx.datastore:datastore-preferences:${Version.datastore}")
 
     // Arch: Room
-    implementation("androidx.room:room-runtime:2.4.0-beta01")
-    kapt("androidx.room:room-compiler:2.4.0-beta01")
-    implementation("androidx.room:room-ktx:2.4.0-beta01")
+    implementation("androidx.room:room-runtime:${Version.room}")
+    kapt("androidx.room:room-compiler:${Version.room}")
+    implementation("androidx.room:room-ktx:${Version.room}")
 
     // Arch: Navigation
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
-    implementation("androidx.navigation:navigation-compose:2.4.0-beta02")
+    implementation("androidx.navigation:navigation-fragment-ktx:${Version.nav}")
+    implementation("androidx.navigation:navigation-ui-ktx:${Version.nav}")
+    implementation("androidx.navigation:navigation-compose:2.4.0-rc01")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-rc01")
 
     // Arch: WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.7.0-alpha04")
-    implementation("androidx.work:work-gcm:2.7.0-alpha04")
+    implementation("androidx.work:work-runtime-ktx:${Version.workmanager}")
+    implementation("androidx.work:work-gcm:${Version.workmanager}")
 
     // Testing: Instrumentation Testing
 
     androidTestImplementation("androidx.test.ext:junit:+")
     androidTestImplementation("androidx.test.espresso:espresso-core:+")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0-beta02")
 
     androidTestImplementation("androidx.test.ext:junit-ktx:1.1.3")
     androidTestImplementation("androidx.test.ext:truth:1.4.0")
     androidTestImplementation("com.google.truth:truth:1.1.3")
 
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.38.1")
-    kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.38.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:${Version.compose}")
+    androidTestImplementation("androidx.arch.core:core-testing:2.1.0")
+
+    androidTestImplementation("com.google.dagger:hilt-android-testing:${Version.hilt}")
+    kaptAndroidTest("com.google.dagger:hilt-android-compiler:${Version.hilt}")
 }
