64e7bce923f95f01d4906fd38e865fcf74b714a5
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-15 17:17:27 +0000

    Removed sharedPreference composable
    Not sure why this is still around
    Since it has been replaced with Datastore
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/util/Compose.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/util/Compose.kt
@@ -1,14 +1,8 @@
 package uk.co.sksulai.multitasker.util
 
 import androidx.compose.runtime.*
-
-import android.content.Context
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.LocalContext
 
-@Composable fun sharedPreferences(name: String, mode: Int = Context.MODE_PRIVATE): SharedPreferences =
-    LocalContext.current.getSharedPreferences(name, mode)
 
 val LocalActivity = staticCompositionLocalOf<AppCompatActivity> {
     error("CompositionLocal LocalActivity not present")
