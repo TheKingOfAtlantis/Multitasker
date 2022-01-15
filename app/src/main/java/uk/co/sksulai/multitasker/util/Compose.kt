f5f43552b26e5534e0f66a37438fe2964703e91c
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-15 20:25:11 +0000

    rememberMutableState(...) Functions
    Pair of wrappers which wrap around remember { mutableStateOf(...) } to just
    make life a little easier
    One version takes vararg of keys which are passed to remember to enable
    recalculation of the mutable state where ever relevant
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/util/Compose.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/util/Compose.kt
@@ -15,3 +15,32 @@ val LocalActivity = staticCompositionLocalOf<AppCompatActivity> {
     LocalActivity provides activity,
     content = block
 )
+
+/**
+ * Simple wrapper to [remember] a [MutableState]
+ *
+ * @param value Initial value to be passed to [mutableStateOf]
+ * @return The remembered mutable state
+ *
+ * @see remember
+ * @see mutableStateOf
+ */
+@Composable fun <T> rememberMutableState(
+    value: T,
+    policy: SnapshotMutationPolicy<T> = structuralEqualityPolicy()
+) = remember { mutableStateOf(value, policy) }
+/**
+ * Simple wrapper to [remember] a [MutableState]
+ *
+ * @param key Given to [remember] to trigger the recalculation of the [MutableState]
+ * @param value Initial value to be passed to [mutableStateOf]
+ * @return The remembered mutable state
+ *
+ * @see remember
+ * @see mutableStateOf
+ */
+@Composable fun <T> rememberMutableState(
+    vararg key: Any?,
+    value: T,
+    policy: SnapshotMutationPolicy<T> = structuralEqualityPolicy()
+) = remember(key) { mutableStateOf(value, policy) }
