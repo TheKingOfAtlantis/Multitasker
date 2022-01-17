214944a9769a9220d935d545b3b295266602f090
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-17 14:58:22 +0000

    Handle Dynamic Links
    Reimplemented dynamic link handling but now we use deep links to navigate to the
    relevant page rather than having it nested within the actions composable
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/ui/EntryPoint.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/EntryPoint.kt
@@ -1,5 +1,8 @@
 package uk.co.sksulai.multitasker.ui
 
+import android.net.Uri
+import android.app.Activity
+import android.content.Intent
 import androidx.compose.runtime.*
 
 import androidx.navigation.*
@@ -8,9 +11,25 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 import androidx.datastore.preferences.core.emptyPreferences
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 
 import uk.co.sksulai.multitasker.ui.screen.signin.*
+import uk.co.sksulai.multitasker.util.*
 import uk.co.sksulai.multitasker.util.DatastoreLocators.AppState
+
+const val MultitaskerBaseUrl = "app.multitasker.xyz"
+
+@Composable private fun handleDynamicLinks(
+    navController: NavHostController,
+    activity: Activity = LocalActivity.current,
+    dynamicLinks: FirebaseDynamicLinks = Firebase.dynamicLinks
+) = dynamicLinks.getDynamicLink(activity.intent).addOnSuccessListener {
+    val deepLink = it?.link
+    if(deepLink != null)
+        navController.handleDeepLink(Intent().apply { data = deepLink })
+}
 
 @Composable fun EntryPoint(
     navController: NavHostController = rememberNavController(),
@@ -22,6 +41,7 @@ import uk.co.sksulai.multitasker.util.DatastoreLocators.AppState
         else -> Destinations.SignInFlow.route
     }
 
+    handleDynamicLinks(navController)
     NavHost(navController, initialRoute) {
         navigation(
             route = Destinations.SignInFlow.route,
@@ -37,5 +57,45 @@ import uk.co.sksulai.multitasker.util.DatastoreLocators.AppState
         }
 
         composable(Destinations.CalendarView.route) { }
+
+        composable(
+            "FirebaseAction?mode={mode}&oobCode={oobCode}&apiKey={apiKey}&continueUrl={continueUrl}&lang={lang}",
+            deepLinks = listOf(
+                NavDeepLink("$MultitaskerBaseUrl/user/action?mode={mode}&oobCode={oobCode}&apiKey={apiKey}&continueUrl={continueUrl}&lang={lang}"),
+            ),
+            arguments = listOf(
+                navArgument("mode")        { },
+                navArgument("oobCode")     { nullable = true },
+                navArgument("apiKey")      { nullable = true },
+                navArgument("lang")        { nullable = true },
+                navArgument("continueUrl") {
+                    defaultValue = null
+                    nullable     = true
+                },
+            )
+        ) {
+            it.arguments?.let { bundle ->
+                val mode:        String by bundle
+                val oobCode:     String by bundle
+                val apiKey:      String by bundle
+                val continueUrl: String by bundle
+                val lang:        String by bundle
+
+                // Where ever we end up we only need the oobCode and continueUrl
+                fun buildUrl(path: String) =
+                    Uri.parse("https://$MultitaskerBaseUrl/user/$path?code=$oobCode&continueUrl=$continueUrl")
+
+                navController.navigate(when(mode) {
+                    "resetPassword" -> buildUrl("reset")
+                    "recoverEmail"  -> buildUrl("recover")
+                    "verifyEmail"   -> buildUrl("verify")
+                    else -> throw IllegalArgumentException("Unknown firebase action mode given: $mode")
+                }, navOptions {
+                    popUpTo(it.destination.id) { // Remove this from the backstack
+                        inclusive = true
+                    }
+                })
+            }
+        }
     }
 }
