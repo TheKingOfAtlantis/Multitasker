214de022aa6fcfa2b71f2ee113445c91ce4ec064
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-19 16:43:44 +0000

    Helper Text Component
    Extends the functionality of ErrorText to show a helper text which can be replaced
    with an error message if provided
    
    Added:
     - Helper text: Shows helper caption or error
     - Default helper padding modifier
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/ui/component/FieldLabel.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/component/FieldLabel.kt
@@ -1,11 +1,25 @@
 package uk.co.sksulai.multitasker.ui.component
 
 import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.error
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+
+/**
+ * Default padding to be applied to helper text under a text field.
+ * It adds 16[dp] to the start of the text and 16[dp] to the top from the baseline
+ *
+ * @see ErrorText
+ * @see HelperText
+ */
+fun Modifier.textFieldHelperPadding() = this
+    .widthIn(max = TextFieldDefaults.MinWidth)
+    .paddingFromBaseline(top = 16.dp)
+    .padding(horizontal = 16.dp)
 
 /**
  * Used to properly show an error message to the user with the correct formatting
@@ -37,3 +51,37 @@ import androidx.compose.ui.text.TextStyle
     label: String,
     isError: Boolean
 ) = Text(label + if(isError) " *" else "")
+
+/**
+ * Helper text of a text field
+ *
+ * @param text     Helper text to be shown under the text field
+ * @param isError  Whether this text is a helper or error message
+ * @param modifier Modifier to apply to this component
+ */
+@Composable fun HelperText(
+    text: String,
+    modifier: Modifier = Modifier,
+    isError: Boolean = false,
+) = ProvideTextStyle(MaterialTheme.typography.caption) {
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        if(isError) ErrorText(text, modifier)
+        else Text(text, modifier)
+    }
+}
+/**
+ * Helper text of a text field
+ *
+ * @param text     Helper text to be shown under the text field
+ * @param error    Error message to be shown in-place of [text] (if not empty)
+ * @param modifier Modifier to apply to this component
+ */
+@Composable fun HelperText(
+    text: String,
+    error: String?,
+    modifier: Modifier
+) = HelperText(
+    text     = error.takeUnless { it.isNullOrEmpty() } ?: text,
+    isError  = !error.isNullOrEmpty(),
+    modifier = modifier
+)
