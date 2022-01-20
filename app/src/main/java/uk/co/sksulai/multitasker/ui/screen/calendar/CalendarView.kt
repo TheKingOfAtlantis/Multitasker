bf8c43a9d906d83990bdfe9539c2990e30c61fe2
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-20 14:38:32 +0000

    Bottom Sheet Shape Animation
    Animate the bottom sheet shape as it transitions from rounded to square corners
    as the height of the sheet approaches the full size of the screen
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/ui/screen/calendar/CalendarView.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/screen/calendar/CalendarView.kt
@@ -1,21 +1,48 @@
 package uk.co.sksulai.multitasker.ui.screen.calendar
 
+import androidx.compose.runtime.*
 import androidx.compose.material.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.*
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.lerp
+
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.hilt.navigation.compose.hiltViewModel
+
 import com.google.accompanist.navigation.material.*
+
 import uk.co.sksulai.multitasker.db.viewmodel.CalendarViewModel
 import uk.co.sksulai.multitasker.ui.Destinations
 
 
+@ExperimentalMaterialApi
+@ExperimentalMaterialNavigationApi
+@Composable fun rememberBottomSheetShape(
+    state: BottomSheetNavigatorSheetState
+): State<RoundedCornerShape> {
+    val animationFraction by run {
+        rememberUpdatedState(when(state.progress.to) {
+            ModalBottomSheetValue.Expanded     -> 1 - state.progress.fraction
+            ModalBottomSheetValue.HalfExpanded -> state.progress.fraction
+            ModalBottomSheetValue.Hidden       ->
+                if(state.progress.from == ModalBottomSheetValue.Expanded)
+                    state.progress.fraction
+                else 1f
+        })
+    }
+    return rememberUpdatedState(RoundedCornerShape(
+        topStart = lerp(0.dp, 24.dp, animationFraction),
+        topEnd   = lerp(0.dp, 24.dp, animationFraction)
+    ))
+}
 
 @OptIn(
+    ExperimentalMaterialApi::class,
     ExperimentalMaterialNavigationApi::class,
 ) @Composable fun CalendarScreen(
     navController: NavHostController,
@@ -23,7 +50,6 @@ import uk.co.sksulai.multitasker.ui.Destinations
 ) {
     val bottomSheetNavigator  = rememberBottomSheetNavigator()
     val calendarNavController = rememberNavController(bottomSheetNavigator)
-
 
     @Composable fun fab() = FloatingActionButton(
         onClick = { Destinations.CalendarCreation.navigate(calendarNavController) },
@@ -37,8 +63,10 @@ import uk.co.sksulai.multitasker.ui.Destinations
     @Composable fun bottomAppBar() = BottomAppBar {
     }
 
+    val shape by rememberBottomSheetShape(bottomSheetNavigator.navigatorSheetState)
     ModalBottomSheetLayout(
-        bottomSheetNavigator
+        bottomSheetNavigator,
+        sheetShape = shape
     ) {
         Scaffold(
             bottomBar = { bottomAppBar() },
