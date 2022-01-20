1b8ccf7a99953971e09a3d59e068a3f2b859f2ac
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-01-20 14:22:11 +0000

    Added Destinations for the Calendar Views
    Added destinations for each of the calendar views + calendar/event creation
    bottom sheet dialogs
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/ui/Destinations.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/Destinations.kt
@@ -2,11 +2,15 @@ package uk.co.sksulai.multitasker.ui
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+
 import androidx.navigation.NavController
 import androidx.navigation.NavOptionsBuilder
 
+import uk.co.sksulai.multitasker.R
 
 /**
  * Provides a strongly typed representation of a navigation route
@@ -63,6 +67,21 @@ object Destinations {
     object Forgot     : Destination("Forgot")     // Forgot password screen
 
     object CalendarView : Destination("Calendar") // The calendar view screen
+    object Dashboard    : ExtendedDestination("Dashboard", R.string.dashboard, Icons.Default.Dashboard)         // Dashboard view
+    object Agenda       : ExtendedDestination("Agenda",    R.string.agenda,    Icons.Default.ViewAgenda)        // Agenda view
+    object Day          : ExtendedDestination("Day",       R.string.day,       Icons.Default.CalendarViewDay)   // Day view
+    object Week         : ExtendedDestination("Week",      R.string.week,      Icons.Default.CalendarViewWeek)  // Week view
+    object Month        : ExtendedDestination("Month",     R.string.month,     Icons.Default.CalendarViewMonth) // Month view
+
     // List of the views in the calendar view
-    val calendarDestinations = listOf<Destination>()
+    val calendarDestinations = listOf(
+        Dashboard,
+        Agenda,
+        Day,
+        Week,
+        Month
+    )
+
+    object CalendarCreation : Destination("calendar_creation")  // Calendar creation dialog
+    object EventCreation    : Destination("event_creation")     // Event creation dialog
 }
