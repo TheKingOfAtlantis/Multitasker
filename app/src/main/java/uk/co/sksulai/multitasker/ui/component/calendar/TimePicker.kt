03391950a37a66bc6340c692c131a6b6406da2e6
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-02-03 23:59:13 +0000

    Time Picker: 24hr Clock
    Added:
     - Support for multiple tracks/rings in a dial => Used for 24hr hour dial
     - Correct text field depending on 12hr vs 24hr clock
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/ui/component/calendar/TimePicker.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/ui/component/calendar/TimePicker.kt
@@ -1,23 +1,19 @@
 package uk.co.sksulai.multitasker.ui.component.calendar
 
-import android.text.format.DateFormat
-import android.text.format.DateUtils
-import androidx.annotation.IntRange
+import kotlin.math.*
+import java.time.format.DateTimeFormatter
 import java.text.NumberFormat
 import java.time.LocalTime
 import java.time.temporal.ChronoField
-import kotlin.math.*
-
+import android.text.format.DateFormat
+import kotlinx.coroutines.flow.collect
 
 import androidx.compose.runtime.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.gestures.*
-import androidx.compose.foundation.interaction.DragInteraction
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.interaction.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.selection.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -34,8 +30,6 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.*
 import androidx.compose.ui.window.*
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
 
 import uk.co.sksulai.multitasker.ui.component.NumberField
 import uk.co.sksulai.multitasker.util.rememberSaveableMutableState
@@ -53,11 +47,16 @@ object TimePicker {
         /**
          * Generic based from which the hour and minute dials built
          *
-         * @param position         The current index position of the dial hand (in range 0..[steps])
+         * @param position The current index position of the dial hand (in range 0..[steps])
          * @param onPositionChange Called when the user moves the dial to a new position
-         * @param steps            The number of steps around the dial
-         * @param labels           Set of labels to be placed at specific positions around the dial
-         * @param modifier         Modifier to be applied to the dial
+         * @param steps    The number of steps around the dial
+         * @param labels   Set of labels to be placed at specific positions around the dial
+         * @param modifier Modifier to be applied to the dial
+         * @param rings    Sets the number of rings to split the steps among, by default all values
+         *                 are on a single ring around the outer edge, however additional rings are
+         *                 added inwards with higher values being further in
+         * @param interactionSource A stream of interactions which can be used to observe the user's
+         *                 interactions with the dial
          */
         @Composable fun Dial(
             position: Int,
@@ -65,6 +64,7 @@ object TimePicker {
             steps: Int,
             labels: Map<Int, String>,
             modifier: Modifier = Modifier,
+            rings: Int = 1,
             interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
         ) {
             if(position !in 0 until steps) throw IndexOutOfBoundsException(
@@ -74,18 +74,34 @@ object TimePicker {
             val diameter      = 256.dp
             val paddingRadius = 16.dp
             val radius        = diameter/2
+            val ringWidth     = 36.dp
 
             val armColour = MaterialTheme.colors.primary
             val dialBackground = LocalContentColor.current
                 .copy(alpha = ContentAlpha.disabled)
 
-            fun getAngle(position: Int) = (2 * PI) * (position.toFloat()/steps)
+            /**
+             * The number of steps in each ring
+             */
+            val stepsPerRing = remember(steps, rings) { steps/rings }
+            /**
+             * Retrieves the ring track index associated with particular [position] index
+             */
+            fun getRing(position: Int) = (position.toFloat()/stepsPerRing).toInt()
+            /**
+             * Retrieves the angle around the dial associated with particular [position] index
+             */
+            fun getAngle(position: Int) =
+                // Divide the number of steps by the number of rings to get the number of divisions
+                // of the dial at each ring level
+                // Then wrap the angle to 360°/2π
+                ((2 * PI) * (position.toFloat()/(steps/rings)) % (2 * PI))
 
             Layout(
                 modifier = modifier.drawBehind {
                     drawCircle(
                         dialBackground,
-                        radius = size.width/2f,
+                        radius = radius.toPx(),
                     )
                 }
                 .pointerInput(Unit) {
@@ -93,13 +109,21 @@ object TimePicker {
                         awaitPointerEventScope {
                             fun updatePosition(position: Offset) {
                                 val newPosition = position - Offset(diameter.toPx(), diameter.toPx()) / 2f
+                                val newAngle    = (newPosition.run { atan2(x, -y) } + 2 * PI) % (2 * PI)
+
+                                // Work out which track the pointer in on
+                                // Then coarse value to ensure we don't try and get value from a non-existent track
+                                val ring = ((radius.toPx() - newPosition.getDistance())/ringWidth.toPx()).toInt()
 
                                 // Ignore the centre
-                                if(newPosition.getDistance() < (radius - 64.dp).toPx())
-                                    return
+                                if(ring > rings - 1) return
 
-                                val newAngle = (newPosition.run { atan2(x, -y) } + 2 * PI) % (2 * PI)
-                                onPositionChange((newAngle / (2 * PI) * steps).roundToInt() % steps)
+                                onPositionChange(run {
+                                    val ringOffset   = ring * stepsPerRing
+                                    val ringPosition = (newAngle/(2 * PI) * stepsPerRing).roundToInt() % stepsPerRing
+
+                                    ringPosition + ringOffset
+                                })
                                 // TODO: Handle haptic feedback
                             }
 
@@ -137,10 +161,11 @@ object TimePicker {
                             .fillMaxSize()
                     ) {
                         val angle = getAngle(position).toFloat()
+                        val track = getRing(position)
                         // TODO: Investigate if labels are truly positioned in a circle and not a
                         //       oval that almost appears as a circle
                         // TODO: Determine the best value for the final arm adjustment
-                        val radius = size.width/2 - paddingRadius.toPx() - 8.dp.toPx()
+                        val radius = size.width/2 - paddingRadius.toPx() - 8.dp.toPx() - ringWidth.toPx() * track
 
                         val armEnd = center + Offset(
                             sin(angle)  * radius,
@@ -197,18 +222,21 @@ object TimePicker {
                     arm.place(IntOffset.Zero)
 
                     labelPlaceables.forEach { (position, placeable) ->
+                        val ring  = getRing(position)
                         val angle = getAngle(position).toFloat()
                         val x = run {
                             val pos = radius * (1 + sin(angle))
                             val sizeCorrection = placeable.width.toDp()  * .5f * (sin(angle - PI.toFloat()) - 1)
+                            val ringOffset = -ringWidth * ring * sin(angle)
                             val padding = - paddingRadius * sin(angle)
-                            pos + sizeCorrection + padding
+                            pos + sizeCorrection + padding + ringOffset
                         }
                         val y = run {
                             val pos = radius * (1 - cos(angle))
                             val sizeCorrection = placeable.height.toDp() * .5f * (cos(angle) - 1)
+                            val ringOffset = -ringWidth * ring * (-cos(angle))
                             val padding = paddingRadius * cos(angle)
-                            pos + sizeCorrection + padding
+                            pos + sizeCorrection + padding + ringOffset
                         }
 
                         placeable.place(
@@ -227,16 +255,32 @@ object TimePicker {
             value: Int,
             onValueChange: (Int) -> Unit,
             modifier: Modifier = Modifier,
-            is24hr: Boolean = DateFormat.is24HourFormat(LocalContext.current),
+            is24hr: Boolean,
             interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
         ) = Dial(
-            // TODO: Respect 12hr vs 24hr
-            position = if(value == 12) 0 else value,
-            steps = 12,
-            onPositionChange = onValueChange,
-            labels = (0..11).associateWith {
-                NumberFormat.getInstance().format(if(it == 0) 12 else it)
+            position = when {
+                value == 12 -> 0
+                is24hr && value == 0 -> 12
+                else -> value
             },
+            steps = if(is24hr) 24 else 12,
+            rings = if(is24hr) 2 else 1,
+            onPositionChange = if(!is24hr) onValueChange else { {
+                onValueChange(when(it) {
+                    0 -> 12
+                    12 -> 0
+                    else -> it
+                })
+            } },
+            labels =
+                if(is24hr) (0..23).associateWith {
+                    DateTimeFormatter.ofPattern("k" ).format(LocalTime.of(when(it) {
+                        0 -> 12
+                        12 -> 0
+                        else -> it
+                    }, 0))
+                }
+                else (0..11).associateWith { DateTimeFormatter.ofPattern("H").format(LocalTime.of(it, 0)) },
             modifier = modifier,
             interactionSource = interactionSource
         )
@@ -421,6 +465,7 @@ object TimePicker {
         onValueSelected: (LocalTime) -> Unit,
         onDismissRequest: () -> Unit,
         title: @Composable () -> Unit = { Text("Select Time") }
+        is24hr: Boolean = DateFormat.is24HourFormat(LocalContext.current),
     ) {
         // TODO: Seems as though there is bug in compose which causes the focus requesters
         //       to be in an invalid state when created like this
@@ -441,7 +486,25 @@ object TimePicker {
             onDismissRequest,
             title = title,
             textField = {
-                NumberField(
+                if(is24hr) NumberField(
+                    modifier = Modifier
+                        .focusRequester(hourFocus)
+                        .size(
+                            width  = 96.dp,
+                            height = 80.dp
+                        ),
+                    label = { Text("Hour") },
+                    value = selection.hour,
+                    onValueChange = {
+                        // if the user provides value > 12 should treat as 24hr time
+                        // Otherwise handle as 12hr time
+                        selection.withHour(it)
+                    },
+                    onFormatError = { value, e -> },
+                    readOnly = editMode == EditMode.Picker,
+                    interactionSource = hourInteractionSource
+                )
+                else NumberField(
                     modifier = Modifier
                         .focusRequester(hourFocus)
                         .size(
@@ -480,14 +543,16 @@ object TimePicker {
                     interactionSource = minuteInteractionSource
                 )
 
-                Spacer(Modifier.padding(12.dp))
+                if(!is24hr) {
+                    Spacer(Modifier.width(12.dp))
 
-                Components.AMPMSelector(
-                    value = if(selection.get(ChronoField.AMPM_OF_DAY) == 0) DayDivision.AM else DayDivision.PM,
-                    onValueSelected = {
-                        selection = selection.with(ChronoField.AMPM_OF_DAY, it.ordinal.toLong())
-                    }
-                )
+                    Components.AMPMSelector(
+                        value = if (selection.get(ChronoField.AMPM_OF_DAY) == 0) DayDivision.AM else DayDivision.PM,
+                        onValueSelected = {
+                            selection = selection.with(ChronoField.AMPM_OF_DAY, it.ordinal.toLong())
+                        }
+                    )
+                }
             },
             dial = {
                 val hourDialInteractionSource = remember { MutableInteractionSource() }
@@ -506,9 +571,13 @@ object TimePicker {
                         onValueChange = { selection = selection.withMinute(it) }
                     )
                     else -> Components.HourDial(
-                        value = selection[ChronoField.HOUR_OF_AMPM],
-                        onValueChange = { selection = selection.with(ChronoField.HOUR_OF_AMPM, it.toLong()) },
-                        interactionSource = hourDialInteractionSource
+                        value = if(is24hr) selection.hour else selection[ChronoField.HOUR_OF_AMPM],
+                        onValueChange = {
+                            selection = if(is24hr) selection.withHour(it)
+                            else selection.with(ChronoField.HOUR_OF_AMPM, it.toLong())
+                        },
+                        interactionSource = hourDialInteractionSource,
+                        is24hr = is24hr
                     )
                 }
             },
