48ea437487ecbf5c29ce7d42477231383ca19823
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-02-08 01:23:55 +0000

    Notifications: Database Version + DI
    Added:
     - Provider for NotificationRuleDao in Dao module
     - Auto-migration specification for v5 => v6 so don't lose existing data
     - Notification entities listed in database entities
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/di/DatabaseModule.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/di/DatabaseModule.kt
@@ -23,8 +23,9 @@ import uk.co.sksulai.multitasker.db.createDatabase
 @InstallIn(
     SingletonComponent::class
 ) @Module object DaoModule {
-    @Provides fun provideUserDao(db: LocalDB)     = db.getUserDao()
-    @Provides fun provideCalendarDao(db: LocalDB) = db.getCalendarDao()
-    @Provides fun provideEventDao(db: LocalDB)    = db.getEventDao()
-    @Provides fun provideTagDao(db: LocalDB)      = db.getTagDao()
+    @Provides fun provideUserDao(db: LocalDB)         = db.getUserDao()
+    @Provides fun provideCalendarDao(db: LocalDB)     = db.getCalendarDao()
+    @Provides fun provideEventDao(db: LocalDB)        = db.getEventDao()
+    @Provides fun provideTagDao(db: LocalDB)          = db.getTagDao()
+    @Provides fun provideNotificationDao(db: LocalDB) = db.getNotificationRuleDao()
 }
