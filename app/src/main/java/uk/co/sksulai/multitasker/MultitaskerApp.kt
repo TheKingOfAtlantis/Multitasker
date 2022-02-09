8c88bdc8978a1a07f0056616b5d30e392920b482
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-02-09 18:33:56 +0000

    Work Manager + HIlt
    Added:
     - Hilt dependency for injecting into workers
     - Hilt Worker factory
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/MultitaskerApp.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/MultitaskerApp.kt
@@ -1,7 +1,19 @@
 package uk.co.sksulai.multitasker
 
+import javax.inject.Inject
+
 import android.app.Application
+
+import androidx.work.Configuration
+import androidx.hilt.work.HiltWorkerFactory
 import dagger.hilt.android.HiltAndroidApp
 
 abstract class BaseMultitaskerApp : Application()
-@HiltAndroidApp class MultitaskerApp : BaseMultitaskerApp()
+@HiltAndroidApp class MultitaskerApp : BaseMultitaskerApp(), Configuration.Provider {
+    @Inject lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
