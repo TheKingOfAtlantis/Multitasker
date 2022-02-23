b33121b6c788b8041bfdbaf9b0070beb8792948b
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-02-23 21:33:35 +0000

    Firebase Error Message Map
    This map was extracted from handleAuthError method to allow us to access the
    various error codes when testing
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/main/java/uk/co/sksulai/multitasker/db/viewmodel/UserViewModel.kt
+++ app/src/main/java/uk/co/sksulai/multitasker/db/viewmodel/UserViewModel.kt
@@ -55,6 +55,27 @@ private fun GoogleIntentLauncher.launch(intent: SavePasswordResult) = launch(int
      * The current user's preferred home page
      */
     val preferredHome = currentUser.map { it?.PreferredHome }
+    companion object {
+        @VisibleForTesting
+        val authErrorMessages = mapOf(
+            "ERROR_INVALID_CUSTOM_TOKEN"                     to "The custom token format is incorrect. Please check the documentation.",
+            "ERROR_CUSTOM_TOKEN_MISMATCH"                    to "The custom token corresponds to a different audience.",
+            "ERROR_INVALID_CREDENTIAL"                       to "The supplied auth credential is malformed or has expired.",
+            "ERROR_INVALID_EMAIL"                            to "The email address is badly formatted.",
+            "ERROR_WRONG_PASSWORD"                           to "The password is invalid or the user does not have a password.",
+            "ERROR_USER_MISMATCH"                            to "The supplied credentials do not correspond to the previously signed in user.",
+            "ERROR_REQUIRES_RECENT_LOGIN"                    to "This operation is sensitive and requires recent authentication. Log in again before retrying this request.",
+            "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" to "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.",
+            "ERROR_EMAIL_ALREADY_IN_USE"                     to "The email address is already in use by another account.",
+            "ERROR_CREDENTIAL_ALREADY_IN_USE"                to "This credential is already associated with a different user account.",
+            "ERROR_USER_DISABLED"                            to "The user account has been disabled by an administrator.",
+            "ERROR_USER_TOKEN_EXPIRED"                       to "The user\'s credential is no longer valid. The user must sign in again.",
+            "ERROR_USER_NOT_FOUND"                           to "There is no user record corresponding to this identifier. The user may have been deleted.",
+            "ERROR_INVALID_USER_TOKEN"                       to "The user\'s credential is no longer valid. The user must sign in again.",
+            "ERROR_OPERATION_NOT_ALLOWED"                    to "This operation is not allowed. You must enable this service in the console.",
+            "ERROR_WEAK_PASSWORD"                            to "The password is invalid it must 6 characters at least",
+        )
+    }
 
     /**
      * Used to correctly show Firebase authentication errors to the user
@@ -71,25 +92,7 @@ private fun GoogleIntentLauncher.launch(intent: SavePasswordResult) = launch(int
         onAuthError: suspend (String) -> Unit
     ) {
         // Copied from StackOverflow: https://stackoverflow.com/a/48503254/3856359
-        val authError = when (err.errorCode) {
-            "ERROR_INVALID_CUSTOM_TOKEN"                     -> "The custom token format is incorrect. Please check the documentation."
-            "ERROR_CUSTOM_TOKEN_MISMATCH"                    -> "The custom token corresponds to a different audience."
-            "ERROR_INVALID_CREDENTIAL"                       -> "The supplied auth credential is malformed or has expired."
-            "ERROR_INVALID_EMAIL"                            -> "The email address is badly formatted."
-            "ERROR_WRONG_PASSWORD"                           -> "The password is invalid or the user does not have a password."
-            "ERROR_USER_MISMATCH"                            -> "The supplied credentials do not correspond to the previously signed in user."
-            "ERROR_REQUIRES_RECENT_LOGIN"                    -> "This operation is sensitive and requires recent authentication. Log in again before retrying this request."
-            "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" -> "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address."
-            "ERROR_EMAIL_ALREADY_IN_USE"                     -> "The email address is already in use by another account."
-            "ERROR_CREDENTIAL_ALREADY_IN_USE"                -> "This credential is already associated with a different user account."
-            "ERROR_USER_DISABLED"                            -> "The user account has been disabled by an administrator."
-            "ERROR_USER_TOKEN_EXPIRED"                       -> "The user\'s credential is no longer valid. The user must sign in again."
-            "ERROR_USER_NOT_FOUND"                           -> "There is no user record corresponding to this identifier. The user may have been deleted."
-            "ERROR_INVALID_USER_TOKEN"                       -> "The user\'s credential is no longer valid. The user must sign in again."
-            "ERROR_OPERATION_NOT_ALLOWED"                    -> "This operation is not allowed. You must enable this service in the console."
-            "ERROR_WEAK_PASSWORD"                            -> "The password is invalid it must 6 characters at least"
-            else -> throw err // We couldn't provide user friendly explanation for exception (Must be something fatal or something new)
-        }
+        val authError = authErrorMessages[err.errorCode] ?: throw err
         // Some error codes should be given as email or password errors instead
         when(err.errorCode) {
             "ERROR_INVALID_EMAIL",
