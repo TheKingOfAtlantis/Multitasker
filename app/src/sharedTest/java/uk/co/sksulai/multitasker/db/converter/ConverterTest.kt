beedae1405eb88d0927d728c18513a262cac05f1
Sulaiman Sulaiman <SKSulai98@gmail.com>
2022-02-23 15:15:07 +0000

    Converter Test: Split components of Test
    Done to make it easier to identify which particular test failed from the callstack
    
    Signed-off-by: Sulaiman Sulaiman <SKSulai98@gmail.com>

--- app/src/sharedTest/java/uk/co/sksulai/multitasker/db/converter/ConverterTest.kt
+++ app/src/sharedTest/java/uk/co/sksulai/multitasker/db/converter/ConverterTest.kt
@@ -8,41 +8,47 @@ sealed class ConverterTest<T, U>(
     private val testData: List<Pair<T?, U?>>
 ) {
     /**
-     * @brief Checks that passing null to a converter returns null (in both directions)
+     * Checks that passing null to a converter returns null (in both directions)
      */
     @Test open fun withNull() {
         // Check that applying:
         //     from(null) == null
         //     to(null)   == null
-        assertThat(converter.from(null)).isEqualTo(null)
-        assertThat(converter.to(null)).isEqualTo(null)
+        withNull_From()
+        withNull_To()
     }
+    open fun withNull_From() = assertThat(converter.from(null)).isEqualTo(null)
+    open fun withNull_To() = assertThat(converter.to(null)).isEqualTo(null)
 
     /**
-     * @brief Ensures conversions produce the correct output
+     * Ensures conversions produce the correct output
      */
     @Test open fun validateConversion() {
         // Check that applying:
         //     from(x) == y
         //     to(y)   == x
         testData.forEach { (lhs, rhs) ->
-            assertThat(converter.from(lhs)).isEqualTo(rhs)
-            assertThat(converter.to(rhs)).isEqualTo(lhs)
+            validateConversion_From(lhs, rhs)
+            validateConversion_To(lhs, rhs)
         }
     }
+    open fun validateConversion_From(lhs: T?, rhs: U?) = assertThat(converter.from(lhs)).isEqualTo(rhs)
+    open fun validateConversion_To(lhs: T?, rhs: U?) = assertThat(converter.to(rhs)).isEqualTo(lhs)
 
     /**
-     * @brief Ensures conversions are strongly reversible - i.e. that calls of
-     *        to(...) followed by from(...), or vice versa, identically reproduce
-     *        the original object's state
+     * Ensures conversions are strongly reversible - i.e. that calls of to(...)
+     * followed by from(...), or vice versa, identically reproduce the original
+     * object's state
      */
     @Test open fun inverse() {
         // Check that applying:
         //     from(to(x)) == x
         //     to(from(y)) == y
         testData.forEach { (lhs, rhs) ->
-            assertThat(converter.from(converter.to(rhs))).isEqualTo(rhs)
-            assertThat(converter.to(converter.from(lhs))).isEqualTo(lhs)
+            inverse_FromThenTo(lhs, rhs)
+            inverse_ToThenFrom(lhs, rhs)
         }
     }
+    open fun inverse_FromThenTo(lhs: T?, rhs: U?) = assertThat(converter.from(converter.to(rhs))).isEqualTo(rhs)
+    open fun inverse_ToThenFrom(lhs: T?, rhs: U?) = assertThat(converter.to(converter.from(lhs))).isEqualTo(lhs)
 }
